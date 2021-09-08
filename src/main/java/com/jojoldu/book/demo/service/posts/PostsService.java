2e1b76b72f4dd67679a06bf6831b15ababa99559
goddl717 <37294326+goddl717@users.noreply.github.com>
2021-09-09 03:16:39 +0900

    add : mustache all 조회, 등록 (function and test)

--- src/main/java/com/jojoldu/book/demo/service/posts/PostsService.java
+++ src/main/java/com/jojoldu/book/demo/service/posts/PostsService.java
@@ -3,12 +3,17 @@ package com.jojoldu.book.demo.service.posts;
 
 import com.jojoldu.book.demo.domain.posts.Posts;
 import com.jojoldu.book.demo.domain.posts.PostsRepository;
+import com.jojoldu.book.demo.web.dto.PostsListResponseDto;
 import com.jojoldu.book.demo.web.dto.PostsResponseDto;
 import com.jojoldu.book.demo.web.dto.PostsSaveRequestDto;
 import com.jojoldu.book.demo.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -34,6 +39,12 @@ public class PostsService {
         Posts entity = postsRepository.findById(id)
                 .orElseThrow(()->new IllegalArgumentException("해당 게시글이 없습니다. id = "+id));
         return new PostsResponseDto(entity);
+    }
 
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
