d5729ad9d3642e070b563442dd07d9cdc154be87
goddl717 <37294326+goddl717@users.noreply.github.com>
2021-09-18 03:58:22 +0900

    add : security test

--- src/test/java/com/jojoldu/book/demo/DemoApplicationTests.java
+++ src/test/java/com/jojoldu/book/demo/DemoApplicationTests.java
@@ -1,10 +1,16 @@
 package com.jojoldu.book.demo;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.context.WebApplicationContext;
+
 
 @SpringBootTest
 class DemoApplicationTests {
+
 
 	@Test
 	void contextLoads() {
